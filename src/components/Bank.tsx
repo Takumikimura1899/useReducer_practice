134fc071f0f8f84eb5ea24e38b149071919d643d
taku1219 <acmiran189926@gmail.com>
2022-03-18 17:07:16 +0900

    [add] useReducerを定義

--- src/components/Bank.tsx
+++ src/components/Bank.tsx
@@ -1,5 +1,25 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+
+type State = {
+  savings: number;
+};
+
+type Action =
+  | { type: 'DEPOSIT'; payload: number }
+  | { type: 'WITHDRAW'; payload: number };
+
+const initialState = {
+  savings: 100000,
+};
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
 
 export const Bank = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return <div>Bank</div>;
 };
